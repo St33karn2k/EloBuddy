a90261064768a8b9a13f0e887869b5d8f06b97eb
elosupport <krxeto@gmail.com>
2016-01-31 17:36:25 +0900

    changed the name

--- AutoBuddy/Properties/AssemblyInfo.cs
+++ AutoBuddy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("AutoBuddy BETA")]
+[assembly: AssemblyTitle("AutoBuddy Edited")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoBuddy BETA")]
+[assembly: AssemblyProduct("AutoBuddy Edited")]
 [assembly: AssemblyCopyright("Copyright Christian Brutal Sniper@Elobuddy  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0106.1848")]
-[assembly: AssemblyFileVersion("1.0.0106.1848")]
+[assembly: AssemblyVersion("2016.01.31.1")]
+[assembly: AssemblyFileVersion("2016.01.31.1")]
